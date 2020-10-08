46962d44aeedaccd5f7c04d78c0fd77f79abf8a3
Arcylen <arcylen.gg@gmail.com>
2020-10-08 10:18:54 +0800

    Seed User

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,7 +14,11 @@ class UserSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        //
+    { 
+        DB::table('users')->insert([
+            'name' => 'Demo Account Seed',
+            'email' => 'demo@demo.com.au',
+            'password' => Hash::make('hello2020'),
+        ]);
     }
 }
